cf9d932baaa38ba41471024fe79e8207fdd8645e
till straumann <till.straumann@alumni.tu-berlin.de>
2020-10-09 00:06:12 +0200

    LedStrip.py: setPwmIref() accepts arrays/collections

--- LedStrip.py
+++ LedStrip.py
@@ -4,9 +4,10 @@ import time
 import math
 
 class LedStrip:
-  def __init__(self, i2c, i2cAddrLeft = 0x69, i2cAddrRight = 0x05):
+  def __init__(self, i2c, i2cAddrLeft = 0x69, i2cAddrRight = 0x05, nLeds=30):
     self.i2cAddrRight_ = i2cAddrRight
     self.i2cAddrLeft_  = i2cAddrLeft
+    self.nLeds_        = nLeds
     self.i2c_          = i2c
     self.ctlLeft_      = bytearray(4)
     self.ctlRight_     = bytearray(4)
@@ -15,10 +16,40 @@ class LedStrip:
     self.ctlLeft_.insert(0, 0x82)
     self.ctlRight_.insert(0, 0x82)
 
-  def setPwdIref(self, pwm, iref):
-    d = [0x88] + [pwm  for i in range(16)] + [iref for i in range(16)]
-    self.i2c_.sendStartWrite( self.i2cAddrRight_, d )
-    self.i2c_.sendStartWrite( self.i2cAddrLeft_ , d )
+  def setPwmIref(self, pwm, iref = None):
+    if pwm is None:
+      if iref is None:
+        return
+      h   = bytearray([0x98])
+      pwm = bytearray()
+    else:
+      h   = bytearray([0x88])
+      try:
+        if 0 == len(pwm):
+          h   = bytearray([0x98])
+        elif self.nLeds_ != len(pwm):
+          raise RuntimeError("LedStrip.setPwmIref(): invalid pwm arg (must have {:d} elements)".format(self.nLeds_))
+      except TypeError:
+        pwm = [pwm for i in range(self.nLeds_)]
+      pwm = bytearray( pwm )
+    if iref is None:
+      iref = bytearray()
+    else:
+      try:
+        if self.nLeds_ != len(iref) and 0 != len(iref):
+          raise RuntimeError("LedStrip.setPwmIref(): invalid iref arg (must have {:d} elements)".format(self.nLeds_))
+      except TypeError:
+        iref = [iref for i in range(self.nLeds_)]
+      iref = bytearray(iref)
+    if self.nLeds_ <= 16:
+      if len(pwm) > 0:
+        pwm.extend([0 for i in range(16 - self.nLeds_)])
+      self.i2c_.sendStartWrite( self.i2cAddrRight_, h + pwm + iref )
+    else:
+      if len(pwm) > 0:
+        pwm.extend([0 for i in range(32 - self.nLeds_)])
+      self.i2c_.sendStartWrite( self.i2cAddrRight_, h + pwm[ 0:16] + iref[0 :16         ] )
+      self.i2c_.sendStartWrite( self.i2cAddrLeft_ , h + pwm[16:32] + iref[16:self.nLeds_] )
 
   @staticmethod
   def _getIdxShft(i):
@@ -71,12 +102,12 @@ class LedStrip:
   def test(self, termCount = (1<<16), sleepTime=0.01):
     self.setCtrl(0xffffffff)
     for i in range(0xa0):
-      self.setPwdIref( 0xff, i )
+      self.setPwmIref( 0xff, i )
       time.sleep(0.01)
     for i in range(0xf0, -1, -1):
-      self.setPwdIref( i, 0xa0 )
+      self.setPwmIref( i, 0xa0 )
       time.sleep(0.01)
-    self.setPwdIref(0x60, 0xc0)
+    self.setPwmIref(0x60, 0xc0)
     for i in range(2):
       self.setCtrl(0x55555555)
       time.sleep(3.0)
